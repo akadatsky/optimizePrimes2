158766a4f3057ff26a389f616c1ff180f495ef20
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2021-04-26 20:08:35 +0300

    Add timer 100k => 9.2s

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,28 @@ package com.company;
 
 public class Main {
 
+    public static final int MAX_COUNT = 100_000;
+    // 100_000 => 9.21 s
+
     public static void main(String[] args) {
         //checkIsPrime();
+
+        int count = 0;
+
+        long time = System.currentTimeMillis();
+
+        for (int i = 2; i < MAX_COUNT; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+
+        time = System.currentTimeMillis() - time;
+
+
+        //System.out.println("Count: " + count);
+        System.out.println("Time: " + time / 1000.0 + " s");
+
     }
 
     public static boolean isPrime(int value) {
